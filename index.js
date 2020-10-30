aa4c8e8c54e62a680c232a7de261bc58a090575c
Baloucarps <Baloucarps@gmail.com>
2020-10-31 03:42:53 +0800

    dsaasddasdsasad

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // NodeJS modules
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 const dir = path.join(__dirname, '/src');
 
 // FOR HEROKU! Heroku Postgres
@@ -18,9 +19,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Express middleware
+app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+// Routes
 app.post('/login', async (req, res) => {
     try {
         const client = await pool.connect();
